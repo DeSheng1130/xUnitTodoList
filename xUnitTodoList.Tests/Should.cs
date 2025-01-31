2202ab15b0f76b1ad996010f949e1549ff1a2b85
steven <a65030814@gmail.com>
2025-01-31 09:47:59 +0800

    new should test function

---
+++ xUnitTodoList.Tests/Should.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace xUnitTodoList.Tests
+{
+    public static class Should
+    {
+        public static void BeNotNull<T>(T obj)
+        {
+            Assert.NotNull(obj);
+        }
+
+        public static void HaveCountOf<T>(IEnumerable<T> collection, int expectedCount)
+        {
+            BeEqualTo(collection.Count(), expectedCount);
+        }
+
+        public static void BeNull<T>(T obj)
+        {
+            Assert.Null(obj);
+        }
+
+        public static void BeEqualTo<T>(T actual, T expected)
+        {
+            Assert.Equal(actual, expected);
+        }
+
+    }
+}
